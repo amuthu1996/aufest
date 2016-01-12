e538bc902b620f3e54281a13069b4387b74b1b53
Senthil Ramasamy <www.senthilr.com@gmail.com>
2016-01-11 19:33:09 -0500

    terms and conditions added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :registers
-  root to: 'visitors#index'
+  root to: 'registers#new'
   devise_for :users
   resources :users
 end
